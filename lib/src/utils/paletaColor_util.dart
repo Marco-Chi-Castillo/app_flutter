e3edc3becac1aceb653e244a5bf095cb5bb5fc56
Marco-Chi-Castillo <chimarcoantonio@gmail.com>
2021-01-17 13:25:14 -0600

    Cambio de Thema

--- lib/src/utils/paletaColor_util.dart
+++ lib/src/utils/paletaColor_util.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Style {
-  static const Color colorPrimary = Color(0xff712634);
+  static const Color colorPrimary = Color(0xff1E379B); //colo rojo: 0xff712634
+  // static const Color colorPrimaryLogin = Color(0xff1E379B);
   static const Color colorEditButton = Color(0xff0F5197);
   static const Color colorDeleteButton = Color(0xffA40707);
   static const Color colorText = Color(0xff898989);
